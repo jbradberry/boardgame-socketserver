9601dfac815ed8cd2bf74d1aa4745f5ddf99842e
Jeff Bradberry <jeff.bradberry@gmail.com>
2013-01-03 15:09:42 -0500

    Filled in a simple setup.py.

---
+++ setup.py
@@ -0,0 +1,11 @@
+from distutils.core import setup
+
+setup(
+    name='BoardServer',
+    version='0.1dev',
+    author='Jeff Bradberry',
+    author_email='jeff.bradberry@gmail.com',
+    packages=['boardserver'],
+    license='LICENSE',
+    description="A generic board game socket server.",
+)
