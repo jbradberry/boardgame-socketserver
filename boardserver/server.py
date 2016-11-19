3ab4a12821c7b1071dba941b2475abc7160fc476
Jeff Bradberry <jeff.bradberry@gmail.com>
2016-11-19 11:52:31 -0500

    Send and accept unpacked json representations over the wire
    This protocol will be easier to deal with by 3rd-party
    implementations, and will be more human-readable.

--- boardserver/server.py
+++ boardserver/server.py
@@ -1,6 +1,7 @@
-import sys
+import json
 import random
-import ast
+import sys
+
 import gevent, gevent.local, gevent.queue, gevent.server
 
 
@@ -16,13 +17,6 @@ class Server(object):
         # random player selection
         self.player_numbers = gevent.queue.JoinableQueue()
 
-        self.sender = {'player': self.send_player,
-                       'update': self.send_update,
-                       'action': self.send_action,
-                       'winners': self.send_winners,
-                       'error': self.send_error,
-                       'illegal': self.send_illegal,}
-
         self.addr = addr if addr is not None else '127.0.0.1'
         self.port = port if port is not None else 4242
 
@@ -30,13 +24,18 @@ class Server(object):
         while True:
             # initialize the game state
             del self.states[:]
-            self.states.append(self.board.starting_state())
+            state = self.board.starting_state()
+            self.states.append(state)
 
-            # update all players with the starting state and mark
-            # player 1 to move
+            # update all players with the starting state
+            state = self.board.unpack_state(state)
+            # board = self.board.get_description()
             for x in xrange(1, self.board.num_players+1):
-                self.players[x].put_nowait(('update', (None, self.states[-1])))
-            self.players[1].put_nowait(('action', ()))
+                self.players[x].put_nowait({
+                    'type': 'update',
+                    'board': None,  # board,
+                    'state': state,
+                })
 
             # randomize the player selection
             players = range(1, self.board.num_players+1)
@@ -62,80 +61,74 @@ class Server(object):
         print "connection:", socket
         self.local.socket = socket
         if self.player_numbers.empty():
-            self.send_decline()
+            self.send({
+                'type': 'decline', 'message': "Game in progress."
+            })
+            socket.close()
             return
 
         self.local.run = True
         self.local.player = self.player_numbers.get()
-        self.sender['player'](self.local.player)
+        self.send({'type': 'player', 'message': self.local.player})
 
-        action, update = None, (None, self.states[-1])
         while self.local.run:
-            action, args = self.players[self.local.player].get()
+            data = self.players[self.local.player].get()
             try:
-                self.sender[action](*args)
-                if action == 'action':
+                self.send(data)
+                if data.get('winners') is not None:
+                    self.local.run = False
+
+                elif data.get('state', {}).get('player') == self.local.player:
                     message = socket.recv(4096)
                     messages = message.rstrip().split('\r\n')
                     self.parse(messages[0]) # FIXME: support for multiple messages
                                             #        or out-of-band requests
-                elif action == 'update':
-                    update = args
             except Exception as e:
                 print e
                 socket.close()
                 self.player_numbers.put_nowait(self.local.player)
-                self.players[self.local.player].put_nowait(('update', update))
-                if action == 'action':
-                    self.players[self.local.player].put_nowait(('action', ()))
+                self.players[self.local.player].put_nowait(data)
                 self.local.run = False
         self.player_numbers.task_done()
 
     def parse(self, msg):
-        if msg.startswith('play '):
-            self.handle_play(ast.literal_eval(msg[5:]))
+        try:
+            data = json.loads(msg)
+            if data.get('type') != 'action':
+                raise Exception
+            self.handle_action(data.get('message'))
+        except Exception:
+            self.players[self.local.player].put({
+                'type': 'error', 'message': msg
+            })
+
+    def handle_action(self, notation):
+        action = self.board.pack_action(notation)
+        if not self.board.is_legal(self.states, action):
+            self.players[self.local.player].put({
+                'type': 'illegal', 'message': notation
+            })
             return
-        self.players[self.local.player].put(('error', (msg,)))
 
-    def handle_play(self, play):
-        if not self.board.is_legal(self.states, play):
-            self.players[self.local.player].put(('illegal', (play,)))
-            return
+        self.states.append(self.board.next_state(self.states[-1], action))
+        state = self.board.unpack_state(self.states[-1])
 
-        self.states.append(self.board.next_state(self.states[-1], play))
-        for x in xrange(1, self.board.num_players+1):
-            self.players[x].put(('update', (play, self.states[-1])))
-
+        data = {
+            'type': 'update',
+            'board': None,
+            'state': state,
+            'last_action': {
+                'player': state['previous_player'],
+                'notation': notation,
+                'sequence': len(self.states),
+            },
+        }
         if self.board.is_ended(self.states):
-            winners = self.board.win_values(self.states)
-            for x in xrange(1, self.board.num_players+1):
-                self.players[x].put(('winners', (winners,)))
-        else:
-            next_player = self.board.current_player(self.states[-1])
-            self.players[next_player].put(('action', ()))
+            data['winners'] = self.board.win_values(self.states)
+            data['points'] = self.board.points_values(self.states)
 
-    def send_player(self, player):
-        self.local.socket.sendall("player {0}\r\n".format(player))
+        for x in xrange(1, self.board.num_players+1):
+            self.players[x].put(data)
 
-    def send_decline(self):
-        self.local.socket.sendall("decline 'Game in progress.'\r\n")
-        self.local.socket.close()
-
-    def send_error(self, msg):
-        self.local.socket.sendall("error {0!r}\r\n".format(msg))
-        self.players[self.local.player].put(('action', ()))
-
-    def send_illegal(self, play):
-        self.local.socket.sendall("illegal {0!r}\r\n".format(play))
-        self.players[self.local.player].put(('action', ()))
-
-    def send_update(self, play, state):
-        self.local.socket.sendall(
-            "update ({0!r}, {1!r})\r\n".format(play, state))
-
-    def send_action(self):
-        self.local.socket.sendall("action\r\n")
-
-    def send_winners(self, winners):
-        self.local.socket.sendall("winners {0}\r\n".format(winners))
-        self.local.run = False
+    def send(self, data):
+        self.local.socket.sendall("{0}\r\n".format(json.dumps(data)))
